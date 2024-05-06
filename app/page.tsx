a989c32ad629f4a6d7fc2b78b9c98291499329b2
viktor_vaughn <emmanuel.plougoulm@gmail.com>
2024-05-06 11:22:14 +0200

    [P4] : update homepage to semantic html

--- app/page.tsx
+++ app/page.tsx
@@ -2,36 +2,72 @@ import Image from "next/image";
 import Link from "next/link";
 
 import styles from "./styles/home.module.css";
+import friendsPic from "@/public/assets/webp/motoki.webp";
+import carPic from "@/public/assets/webp/remi.webp";
+
+import TriangleIcon from "@/public/assets/icons/triangle.svg";
+import CircleIcon from "@/public/assets/icons/circle.svg";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.container}>
-        <div className={styles["left-pic"]}>
-          {/* <Image
-            src="/assets/home__image_1.png"
-            width={500}
-            height={500}
-            alt="Picture of the author"
-          /> */}
-        </div>
-        <div className={styles["cities"]}>
+      <div className={styles["grid"]}>
+        <section className={styles["grid__friends-pic"]}>
+          <Image
+            src={friendsPic}
+            layout="fill"
+            objectFit="cover"
+            alt="friends_photo"
+          />
+        </section>
+
+        <aside className={styles["grid__cities-container"]}>
           <div>Paris</div>
-          <div>Sr-Étienne</div>
+          <div>St-Étienne</div>
           <div>Marseille</div>
+        </aside>
+
+        <div className={styles["arrow-icon-one"]}>
+          <Image
+            src={TriangleIcon}
+            width={15}
+            height={15}
+            alt="triangle-icon"
+          />
         </div>
-        <div className={styles["collectif"]}>
-          <div className={styles["title"]}>collectif p4</div>
-          <div className={styles["quote"]}>Collectif joyeusement foutraque</div>
+
+        <section className={styles["grid__title-container"]}>
+          <h1>collectif p4</h1>
+          <h2>Collectif joyeusement foutraque</h2>
+        </section>
+
+        <div className={styles["circle-icon"]}>
+          <Image
+            src={CircleIcon}
+            width={15}
+            height={15}
+            alt="group of people photo"
+          />
         </div>
-        <div className={styles["right-pic"]}></div>
-        <div className={styles["enter"]}>
-          <div>
-            <span className={styles["button"]}>icon</span>
-            <Link className={styles[""]} href="/pages/landing">
-              Entrer
-            </Link>
-          </div>
+
+        <div className={styles["grid__car-pic"]}>
+          <Image
+            src={carPic}
+            layout="fill"
+            objectFit="cover"
+            alt="Picture of the author"
+          />
+        </div>
+        <div className={styles["grid__menu"]}>
+          <Image
+            src={TriangleIcon}
+            width={15}
+            height={15}
+            alt="triangle_icon"
+          />
+          <Link className={styles["enter-button"]} href="/pages/landing">
+            Entrer
+          </Link>
         </div>
       </div>
     </main>
