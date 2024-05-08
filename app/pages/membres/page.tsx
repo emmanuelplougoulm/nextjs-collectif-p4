a44d4a1fb40af2b1ee321761329193ccb25c5b28
viktor_vaughn <emmanuel.plougoulm@gmail.com>
2024-05-08 15:41:16 +0200

    [P4] refact: membres page to flexbox

--- app/pages/membres/page.tsx
+++ app/pages/membres/page.tsx
@@ -1,38 +1,69 @@
-import DefaultLayout from "../../layouts/DefaultLayout";
+"use client";
+import Image from "next/image";
+import config from "./config";
 import styles from "./membres.module.css";
+import DefaultLayout from "../../layouts/DefaultLayout";
+import List from "../../components/list/list";
+
+import { useState } from "react";
+
+// choose type over interface here
+// interface are meant to be extended
+
+interface Membre {
+  name: string;
+  position: string[];
+  cie: string[];
+  imageName: string;
+}
 
 export default function Membres() {
+  const [current, setCurrent] = useState<Membre>(config[0]);
+
+  // make it a custom hook
+  const handleMouseEnter = (item: Membre) => {
+    setCurrent(item);
+  };
+
+  const BASE_LOAD_URL = `/assets/webp/members/`;
+
   return (
     <DefaultLayout>
-      <div className={styles["zone-a"]}>image membre</div>
-      <div className={styles["zone-b"]}>membres</div>
-      <div className={styles["zone-c"]}>
-        <ol>
-          <li>Chloé Chicky</li>
-          <li>Pauline Laurendeau</li>
-          <li>Mathias Minne</li>
-          <li>Arthur Viadieu</li>
-          <li>Roma Blanchard</li>
-          <li>Marie-Line Vergnaux</li>
-          <li>Bob Levasseur</li>
-          <li>Anaïs Ancel</li>
-          <li>Claire Olier</li>
-          <li>Régis Fortino</li>
-          <li>Sarah Calcine</li>
-          <li>Antoine Mermet</li>
-        </ol>
-      </div>
-      <div className={styles["zone-d"]}>
-        <div className={styles["roles"]}>
-          <div>comédienne</div>
-          <div>metteuse en scène</div>
+      <div className={styles["page-container"]}>
+        <div className={styles["text-container"]}>
+          <div className={styles[""]}>
+            <div className={styles["section-title"]}>membres</div>
+            <List className={styles["names-list"]}>
+              {config.map((item) => (
+                <li onMouseEnter={() => handleMouseEnter(item)} key={item.name}>
+                  {item.name}
+                </li>
+              ))}
+            </List>
+          </div>
+          <div className={styles[""]}>
+            <List className={styles["roles-list"]}>
+              {current?.position?.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </List>
+            <List className={styles["cie-list"]}>
+              {current?.cie?.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </List>
+          </div>
         </div>
-        <div className={styles["cie"]}>
-          <div>Conservatoire du 11e (Paris)</div>
-          <div>Cie Luce</div>
-          <div>Cie 5ème acte</div>
+
+        <div className={styles["img-container"]}>
+          <Image
+            priority={true}
+            src={`${BASE_LOAD_URL}${current.imageName}`}
+            layout="fill"
+            objectFit="cover"
+            alt={current.name}
+          />
         </div>
-        <div></div>
       </div>
     </DefaultLayout>
   );
