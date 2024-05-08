b095146c5385bc86dee04cf8e31cb739761e823f
viktor_vaughn <emmanuel.plougoulm@gmail.com>
2024-05-08 16:04:14 +0200

    [P4] refact: spectacles page to flexbox

--- app/pages/spectacles/page.tsx
+++ app/pages/spectacles/page.tsx
@@ -1,12 +1,38 @@
+"use client";
 import styles from "./spectacles.module.css";
 import DefaultLayout from "../../layouts/DefaultLayout";
+import config from "./config";
+
+import { useState } from "react";
 
 export default function Spectacles() {
+  const [current, setCurrent] = useState(config[0]);
+
+  const handleMouseEnter = (item: any) => {
+    setCurrent(item);
+  };
+
   return (
     <DefaultLayout>
-      <div className={styles["zone-a"]}>spectacles</div>
-      <div className={styles["zone-b"]}>liste spectacles</div>
-      <div className={styles["zone-c"]}>spectacle content</div>
+      <div className={styles["page-container"]}>
+        <div className={styles["list-container"]}>
+          <div className={styles["section-title"]}>spectacles</div>
+          <div className={styles["shows-list"]}>
+            <ul>
+              <li onMouseEnter={handleMouseEnter}>
+                J’aurais voulu être Jeff Bezos
+              </li>
+              <li>Le dernier ferme la porte</li>
+              <li> Seule la queue du castor peut tarir la rivière</li>
+              <li>Ma soeur, tes lèvres sont de porcelaine</li>
+              <li> Insoutenables longues étreintes</li>
+            </ul>
+          </div>
+        </div>
+        <div className={styles["content-container"]}>
+          <div className={styles["zone-c"]}>{current.name}</div>
+        </div>
+      </div>
     </DefaultLayout>
   );
 }
