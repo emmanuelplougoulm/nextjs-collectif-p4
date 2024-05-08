f774e614020134ff0b94b141fe82e89793f5087d
viktor_vaughn <emmanuel.plougoulm@gmail.com>
2024-05-08 15:03:09 +0200

    [P4] refact: manifeste page to flexbox

--- app/pages/manifeste/page.tsx
+++ app/pages/manifeste/page.tsx
@@ -1,29 +1,56 @@
-// import Image from "next/image";
+import Image from "next/image";
+import Title from "../../components/title/title";
+import TriangleIcon from "../../components/icons/triangle.svg";
+
 import styles from "./manifeste.module.css";
 import DefaultLayout from "../../layouts/DefaultLayout";
+import manifestePic from "@/public/assets/webp/manifeste.webp";
 
 export default function Manifesto() {
   return (
     <DefaultLayout>
-      <div className={styles["zone-a"]}>image</div>
-      <div className={styles["zone-b"]}>MANIFESTE</div>
-      <div className={styles["zone-c"]}>
-        P4 pour un paquet de cigarette oublié sur le bitume renfermant un mot
+      <div className={styles["page-container"]}>
+        <div className={styles["text-content"]}>
+          <div className={styles["title-container"]}>
+            <Title tag="h3" className={styles["h3"]}>
+              manifeste
+            </Title>
+            <Image
+              src={TriangleIcon}
+              width={15}
+              height={15}
+              alt="triangle-icon"
+            />
+          </div>
+
+          <div className={styles["paragraph"]}>
+            {`P4 pour un paquet de cigarette oublié sur le bitume renfermant un mot
         d’amour, pour un laboratoire de recherche sur la liberté. Pour ne jamais
         avoir à chanter « tiens, voilà du boudin ». C’est une quête de lumière.
         C’est rouler en vieille décapotable en faisant l'avion avec ton bras .
         C’est aussi croire en l'esprit de camaraderie et à la confrontation
         d'idées. C’est dedans et dehors, pour vous et pour nous. C’est raconter
         des histoires, hors des sentiers rebattus, avec générosité et appétit
-        pour le jeu.
-      </div>
-      <div className={styles["zone-d"]}>
-        "Le fossé entre le théâtre pauvre et le théâtre riche est encore élargi,
+        pour le jeu.`}
+          </div>
+
+          <div className={styles["quote"]}>
+            {`"Le fossé entre le théâtre pauvre et le théâtre riche est encore élargi,
         la classe moyenne du théâtre disparaît, un peu d’argent ne suffit plus,
         il en faut beaucoup ou alors, quoi, rien. Il faut trouver l’esthétique
         de ce rien. Qui n’est pas du tout rien, puisqu’il y a les acteurs,
-        porteurs de la totalité des signes du spectacle".
-        <div>A. Vittez</div>
+        porteurs de la totalité des signes du spectacle"`}
+            <div>A. Vittez</div>
+          </div>
+        </div>
+        <div className={styles["img-content"]}>
+          <Image
+            src={manifestePic}
+            layout="fill"
+            objectFit="cover"
+            alt="lightbeams_trees"
+          />
+        </div>
       </div>
     </DefaultLayout>
   );
