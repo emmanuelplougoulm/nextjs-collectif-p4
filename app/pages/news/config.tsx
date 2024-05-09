82404f4e55e10bb2524d5627f83712598c5124fc
viktor_vaughn <emmanuel.plougoulm@gmail.com>
2024-05-09 17:27:35 +0200

    [P4] : add config files

---
+++ app/pages/news/config.tsx
@@ -0,0 +1,20 @@
+const config = [
+  {
+    title: "j'aurais voulu être Jeff Bezos",
+    infos: [
+      "Avignon 2024: Théâtre le 11",
+      "Juin 2024 - Théâtre de l'oeuvre Marseille",
+      "25 mai 2024: Festival de Coye-la-forêt",
+      "Février et Octobre 2023 : Théâtre de Belleville",
+    ],
+  },
+  {
+    title: "création in situ & immersive",
+    infos: [
+      "Avril 24 : Théâtre de l'Oeuvre - Marseille",
+      "Juillet 24: Avignon 2024 a suivre.",
+    ],
+  },
+];
+
+export default config;
---
+++ app/pages/spectacles/config.tsx
@@ -0,0 +1,66 @@
+const config = [
+  {
+    name: "J'aurais voulu être Jeff Bezos",
+    place:
+      "Mention spéciale du jury Prix Jeunes metteurs en scène du Théâtre 13",
+    date: "2021",
+    description:
+      "Jeff Bezos est le fondateur d’Amazon. Derrière la vitrine d’un service satisfaisant instantanément n’importe quelle pulsion consumériste se cache un empire technologique inédit. De l’intelligence artificielle à la robotique en passant par « le divertissement à l’infini » et la conquête spatiale, Jeff Bezos est un homme très occupé. A la croisée du Saturday night live et du théâtre contestataire, J'aurais voulu être Jeff Bezos est un tourbillon satirique et festif qui s’appuie sur des faits documentés. Alexandrins, théâtre de boulevard, et autres surprises viennent distiller une réflexion corrosive autour de cet inquiétant personnage et son hydre tentaculaire.",
+    quotes: [
+      {
+        text: '"Un feu d’artifice satirique et corrosif, efficace et plein de drôlerie."',
+        media: "Piano Panier",
+        url: "http://pianopanier.com/jaurais-voulu-etre-jeff-bezos/",
+      },
+      {
+        text: '""Joyeux patchwork, les comédiens jonglent avec multiples genres : alexandrins, témoignages, pièce basculant dans le vaudeville. Avec humour et énergie, les comédiens nous surprennent. Pièce à multiples facettes, innovante et surprenante.""',
+        media: "État-critique",
+        url: "http://pianopanier.com/jaurais-voulu-etre-jeff-bezos/",
+      },
+      {
+        text: "Un texte et une mise en scène agréablement déconcertants où se croisent l'alexandrin, le vaudeville, l'absurde et l'ultra-réalisme. Cette façon ludique d'aborder une actualité qui devrait faire froid dans le dos, occasionne des numéros détonnants aux comédiens, tous inspirés et finement choisis",
+        media: "Le journal du dimanche",
+      },
+      {
+        text: "Un réquisitoire survolté porté par 5 comédiens exceptionnels (...) Les 5 acteurs qui interprètent tour à tour le rôle de Jeff Bezos mais aussi une grande variété de rôles sont tous épatants, de drôlerie, de justesse et de panache. Le rythme est endiablé de bout en bout et réserve de multiples surprises.",
+        media: "Culture-Tops",
+      },
+      {
+        text: "Un texte et une mise en scène totalement déjantés, servis par des comédiens et comédiennes diablement talentueux qui s’attachent à nous dérouter en permanence",
+        media: "La Souris Scène",
+        url: "https://lasouriscene.fr/theatre/jaurais-voulu-etre-jeff-bezos-2/",
+      },
+    ],
+    demoUrl: "https://www.youtube.com/watch?v=MPLQDtOdZZ8",
+    images: [""],
+  },
+  {
+    name: "LE DERNIER FERME LA PORTE",
+    place: "Le couvent Levat – Spectacle joué à Marseille",
+    date: "Août 2021",
+    description:
+      "Une élue à la culture accueille le public venu voir la nouvelle création du collectif. Le spectacle n'est pas prêt. Elle est kidnappée pour être offerte en sacrifice. La troupe est en fait une secte complètement givrée. Un couvent, un jardin, des couloirs, une chapelle.",
+    demoUrl: "https://www.youtube.com/watch?v=J0eeaM7c6kg",
+    images: [""],
+  },
+  {
+    name: "SEULE LA QUEUE DU CASTOR PEUT TARIR LA RIVIÈRE",
+    place: "CREATION COLLECTIVE IN SITU ET IMMERSIVE",
+    date: "Juillet 2020",
+    description:
+      "Invité en résidence pour le festival « ITINERAIRE BIS » de la fontaine Minérale, P4 crée et joue ce spectacle en Juillet 2020. Masqué, le public suit Charlie, jeune femme venant s’installer dans une communauté New Age des années 80. Une déambulation joyeusement foutraque, parfois glauque mais toujours amusante dans des lieux aussi éclectiques que magnifiques : bar, rivière, chapiteau déglingué, camping abandonné, chemins sinueux, voiture, vieux camping car et forêt.",
+    demoUrl: "https://vimeo.com/505564858",
+    images: [""],
+  },
+  {
+    name: "MA SOEUR, TES LÈVRES SONT DE PORCELAINE",
+    place: ["Marseille (2019)", "Paris 11e (2018)"],
+    date: "Juillet 2020",
+    description:
+      "Ma soeur, tes lèvres sont de porcelaine Première création immersive de P4 guide le spectateur dans un parcours escarpé à partir d'une laverie publique, point de départ d'une déambulation qui nous fait découvrir tour à tour les restes d’une ancienne costumerie de cabaret, un appartement familial, une pizzeria fermée, une chapelle abandonnée.",
+    demoUrl: "https://vimeo.com/509780969",
+    images: [""],
+  },
+];
+
+export default config;
