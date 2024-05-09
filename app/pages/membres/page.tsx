7e3ea41023f4e53e5eb5a727651525e9ecaaf13b
viktor_vaughn <emmanuel.plougoulm@gmail.com>
2024-05-09 12:00:33 +0200

    [P4] styling: update responsive for member page

--- app/pages/membres/page.tsx
+++ app/pages/membres/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 import Image from "next/image";
-import config from "./config";
-import styles from "./membres.module.css";
 import DefaultLayout from "../../layouts/DefaultLayout";
 import List from "../../components/list/list";
+import Title from "../../components/title/title";
+import Container from "../../components/container/container";
+
+import config from "./config";
+import styles from "./membres.module.css";
 
 import { useState } from "react";
 
@@ -29,10 +32,13 @@ export default function Membres() {
 
   return (
     <DefaultLayout>
-      <div className={styles["page-container"]}>
-        <div className={styles["text-container"]}>
-          <div className={styles[""]}>
-            <div className={styles["section-title"]}>membres</div>
+      <Container className={styles["page-container"]}>
+        <section className={styles["text-section"]}>
+          <Title tag="h2" className={styles["section-title"]}>
+            membres
+          </Title>
+
+          <Container className={styles["lists-container"]}>
             <List className={styles["names-list"]}>
               {config.map((item) => (
                 <li onMouseEnter={() => handleMouseEnter(item)} key={item.name}>
@@ -40,22 +46,23 @@ export default function Membres() {
                 </li>
               ))}
             </List>
-          </div>
-          <div className={styles[""]}>
-            <List className={styles["roles-list"]}>
-              {current?.position?.map((item, index) => (
-                <li key={index}>{item}</li>
-              ))}
-            </List>
-            <List className={styles["cie-list"]}>
-              {current?.cie?.map((item, index) => (
-                <li key={index}>{item}</li>
-              ))}
-            </List>
-          </div>
-        </div>
 
-        <div className={styles["img-container"]}>
+            <Container className={styles["infos-container"]}>
+              <List className={styles["roles-list"]}>
+                {current?.position?.map((item, index) => (
+                  <li key={index}>{item}</li>
+                ))}
+              </List>
+              <List className={styles["cie-list"]}>
+                {current?.cie?.map((item, index) => (
+                  <li key={index}>{item}</li>
+                ))}
+              </List>
+            </Container>
+          </Container>
+        </section>
+
+        <Container className={styles["img-container"]}>
           <Image
             priority={true}
             src={`${BASE_LOAD_URL}${current.imageName}`}
@@ -63,8 +70,8 @@ export default function Membres() {
             objectFit="cover"
             alt={current.name}
           />
-        </div>
-      </div>
+        </Container>
+      </Container>
     </DefaultLayout>
   );
 }
