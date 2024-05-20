c9a3242c871439d025d54f505e2c8d2513eb03af
viktor_vaughn <emmanuel.plougoulm@gmail.com>
2024-05-20 19:53:13 +0200

    [P4]: update spectacles config file

--- app/pages/spectacles/config.tsx
+++ app/pages/spectacles/config.tsx
@@ -1,9 +1,14 @@
 const config = [
   {
+    images: ["/images/spectacles/initial/guinguette.jpg"],
+  },
+  {
     name: "J'aurais voulu être Jeff Bezos",
-    place:
-      "Mention spéciale du jury Prix Jeunes metteurs en scène du Théâtre 13",
-    date: "2021",
+    dateAndCity: "Paris - 2021",
+    place: [
+      "Mention spéciale du jury",
+      "Prix Jeunes metteurs en scène du Théâtre 13",
+    ],
     description:
       "Jeff Bezos est le fondateur d’Amazon. Derrière la vitrine d’un service satisfaisant instantanément n’importe quelle pulsion consumériste se cache un empire technologique inédit. De l’intelligence artificielle à la robotique en passant par « le divertissement à l’infini » et la conquête spatiale, Jeff Bezos est un homme très occupé. A la croisée du Saturday night live et du théâtre contestataire, J'aurais voulu être Jeff Bezos est un tourbillon satirique et festif qui s’appuie sur des faits documentés. Alexandrins, théâtre de boulevard, et autres surprises viennent distiller une réflexion corrosive autour de cet inquiétant personnage et son hydre tentaculaire.",
     quotes: [
@@ -33,50 +38,49 @@ const config = [
     ],
     demoUrl: "https://www.youtube.com/embed/MPLQDtOdZZ8?si=xBH-ZXMTi45olKR3",
     images: [
-      "/assets/webp/spectacles/bezos/1.webp",
-      "/assets/webp/spectacles/bezos/2.webp",
-      "/assets/webp/spectacles/bezos/2.webp",
-      "/assets/webp/spectacles/bezos/3.webp",
-      "/assets/webp/spectacles/bezos/4.webp",
-      "/assets/webp/spectacles/bezos/5.webp",
-      "/assets/webp/spectacles/bezos/6.webp",
-      "/assets/webp/spectacles/bezos/7.webp",
-      "/assets/webp/spectacles/bezos/8.webp",
-      "/assets/webp/spectacles/bezos/9.webp",
-      "/assets/webp/spectacles/bezos/10.webp",
-      "/assets/webp/spectacles/bezos/11.webp",
-      "/assets/webp/spectacles/bezos/12.webp",
-      "/assets/webp/spectacles/bezos/13.webp",
-      "/assets/webp/spectacles/bezos/14.webp",
-      "/assets/webp/spectacles/bezos/15.webp",
+      "/images/spectacles/bezos/1.webp",
+      "/images/spectacles/bezos/2.webp",
+      "/images/spectacles/bezos/3.webp",
+      "/images/spectacles/bezos/4.webp",
+      "/images/spectacles/bezos/5.webp",
+      "/images/spectacles/bezos/6.webp",
+      "/images/spectacles/bezos/7.webp",
+      "/images/spectacles/bezos/8.webp",
+      "/images/spectacles/bezos/9.webp",
+      "/images/spectacles/bezos/10.webp",
+      "/images/spectacles/bezos/11.webp",
+      "/images/spectacles/bezos/12.webp",
+      "/images/spectacles/bezos/13.webp",
+      "/images/spectacles/bezos/14.webp",
+      "/images/spectacles/bezos/15.webp",
     ],
   },
   {
     name: "Le dernier ferme la porte",
-    place: "Le couvent Levat – Spectacle joué à Marseille",
-    date: "Août 2021",
+    dateAndCity: "Marseille - 2021",
+    place: ["Le couvent Levat"],
     description:
       "Une élue à la culture accueille le public venu voir la nouvelle création du collectif. Le spectacle n'est pas prêt. Elle est kidnappée pour être offerte en sacrifice. La troupe est en fait une secte complètement givrée. Un couvent, un jardin, des couloirs, une chapelle.",
     demoUrl: "https://www.youtube.com/watch?v=J0eeaM7c6kg",
-    images: [""],
+    images: ["/images/spectacles/dernier/disco_ball.webp"],
   },
   {
     name: "Seule la queue du castor peut tarir la rivière",
-    place: "CREATION COLLECTIVE IN SITU ET IMMERSIVE",
-    date: "Juillet 2020",
+    dateAndCity: "La fontaine Minérale - 2020",
+    place: ["Création collective In Situ et Immersive"],
     description:
       "Invité en résidence pour le festival « ITINERAIRE BIS » de la fontaine Minérale, P4 crée et joue ce spectacle en Juillet 2020. Masqué, le public suit Charlie, jeune femme venant s’installer dans une communauté New Age des années 80. Une déambulation joyeusement foutraque, parfois glauque mais toujours amusante dans des lieux aussi éclectiques que magnifiques : bar, rivière, chapiteau déglingué, camping abandonné, chemins sinueux, voiture, vieux camping car et forêt.",
     demoUrl: "https://vimeo.com/505564858",
-    images: [""],
+    images: ["/images/spectacles/castor/tail.webp"],
   },
   {
     name: "Ma soeur, tes lèvres sont de porcelaine",
-    place: ["Marseille (2019)", "Paris 11e (2018)"],
-    date: "Juillet 2020",
+    dateAndCity: "Marseille (2019) - Paris (2018)",
+    place: [],
     description:
       "Ma soeur, tes lèvres sont de porcelaine Première création immersive de P4 guide le spectateur dans un parcours escarpé à partir d'une laverie publique, point de départ d'une déambulation qui nous fait découvrir tour à tour les restes d’une ancienne costumerie de cabaret, un appartement familial, une pizzeria fermée, une chapelle abandonnée.",
     demoUrl: "https://vimeo.com/509780969",
-    images: [""],
+    images: ["/images/spectacles/porcelaine/masoeur.webp"],
   },
 ];
 
