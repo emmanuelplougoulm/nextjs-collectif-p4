1e3776ceae4f3cf672f9986b74bf1ae1d1900da6
viktor_vaughn <emmanuel.plougoulm@gmail.com>
2024-05-22 18:22:49 +0200

    [P4]: add styles for membres page

--- app/pages/membres/page.tsx
+++ app/pages/membres/page.tsx
@@ -32,7 +32,7 @@ export default function Membres() {
     <DefaultLayout>
       <Container className={styles["page-container"]}>
         <section className={styles["text-section"]}>
-          <Title tag="h3" className={styles["section-title"]}>
+          <Title tag="h3" className={`${styles["title"]} section-title`}>
             {text.membres.title}
           </Title>
 
