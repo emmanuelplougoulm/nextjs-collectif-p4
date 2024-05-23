aaeabcdf7a0aef347aa1f8d6b673640ab486277e
viktor_vaughn <emmanuel.plougoulm@gmail.com>
2024-05-23 11:02:44 +0200

    [P4]: fix CSS on landing page + add semantic html

--- app/pages/landing/page.tsx
+++ app/pages/landing/page.tsx
@@ -1,25 +1,28 @@
 import Image from "next/image";
 
 import styles from "./landing.module.css";
-import DefaultLayout from "../../layouts/default-layout";
-import pic from "@/public/assets/webp/landing.webp";
+import DefaultLayout from "@/app/layouts/default-layout";
+import comediansPic from "@/public/images/landing.jpg";
 
 import Container from "../../components/shared/container/container";
 import Title from "../../components/shared/title/title";
 import Triangle from "../../components/icons/triangle.svg";
+import Circle from "../../components/icons/circle.svg";
+
+import text from "@/locales/fr/all.json";
 
 export default function Landing() {
   return (
     <DefaultLayout>
-      <Container className={styles["page-container"]}>
-        <section className={styles["content-top"]}>
+      <main className={styles["main"]}>
+        <section className={styles["top-section"]}>
           <Title tag="h2" className={styles["small-title"]}>
             Collectif tout-terrain racontant des histoires avec un filtre <br />
             de poésie, d&apos;humour, de rage et de folie.
           </Title>
           <Image src={Triangle} width={15} height={15} alt="triangle-icon" />
         </section>
-        <section className={styles["content-middle"]}>
+        <section className={styles["middle-section"]}>
           <Container className={styles["large-title-container"]}>
             <Title tag="h1" className={styles["large-title"]}>
               Pour un théâtre <br /> en salle et ailleurs,
@@ -29,20 +32,18 @@ export default function Landing() {
           <Container className={styles["img-container"]}>
             <Image
               fill
-              src={pic}
+              src={comediansPic}
               style={{ objectFit: "cover" }}
               className={styles["shift-down"]}
               alt="two_comedians_laughing"
             />
           </Container>
         </section>
-        <section className={styles["content-bottom"]}>
-          <div className={styles["copyright"]}>@2024 - Collectif P4</div>
-          {/* <div className={styles["img-container"]}>
-            <Image src={Circle} width={15} height={15} alt="triangle-icon" />
-          </div> */}
+        <section className={styles["bottom-section"]}>
+          <div className={styles["copyright"]}>{text.landing.copyright}</div>
+          <Image src={Circle} width={15} height={15} alt="triangle_icon" />
         </section>
-      </Container>
+      </main>
     </DefaultLayout>
   );
 }
