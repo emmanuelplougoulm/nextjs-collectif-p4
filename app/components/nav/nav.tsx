2b6caf21aa922e2c738c727c0268dfc65981cd50
viktor_vaughn <emmanuel.plougoulm@gmail.com>
2024-05-23 16:48:01 +0200

    [P4]: add class to nav when active

--- app/components/nav/nav.tsx
+++ app/components/nav/nav.tsx
@@ -1,32 +1,58 @@
+"use client";
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import styles from "./nav.module.css";
 
 type NavProps = {
   className?: string;
 };
 const Nav: React.FC<NavProps> = ({ className }) => {
-  // const [mounted, setMounted] = useState(false);
-  // const router = useRouter();
+  const [activePath, setActivePath] = useState("");
 
-  // useEffect(() => {
-  //   setMounted(true);
-  // }, []);
+  useEffect(() => {
+    setActivePath(window.location.pathname);
+  }, []);
 
-  // if (!mounted) {
-  //   return null;
-  // }
+  const navItems = [
+    {
+      name: "Manifeste",
+      href: "/pages/manifeste",
+    },
+    {
+      name: "Membres",
+      href: "/pages/membres",
+    },
+    {
+      name: "Actualités",
+      href: "/pages/actualites",
+    },
+    {
+      name: "Spectacles",
+      href: "/pages/spectacles",
+    },
+    {
+      name: "Contact",
+      href: "/pages/contact",
+    },
+  ];
+
+  const isActive = (location: string) => {
+    return activePath === location ? styles.selected : "";
+  };
 
   return (
     <div>
-      <nav className={`${styles["nav"]} ${className}`}>
+      <nav className={`${styles["nav"]} ${className || ""}`}>
         <ol>
-          <Link href="/pages/manifeste">Manifeste</Link>
-          <Link href="/pages//membres">Membres</Link>
-          <Link href="/pages//actualites">Actualités</Link>
-          <Link href="/pages//spectacles">Spectacles</Link>
-          <Link href="/pages//contact">Contact</Link>
+          {navItems.map((item) => (
+            <Link
+              key={item.name}
+              className={isActive(item.href)}
+              href={item.href}
+            >
+              {item.name}
+            </Link>
+          ))}
         </ol>
       </nav>
     </div>
